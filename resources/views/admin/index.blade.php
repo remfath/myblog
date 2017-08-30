af0e328fa7c1a7d70056237c00602de04f2bda05
remfath <remfath@gmail.com>
2017-08-30 13:50:42 +0800

    Update layout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,20 +5,21 @@
         <tr>
             <th style="text-align: center;">标题</th>
             <th width="100" style="text-align: center;">分类</th>
-            <th width="150" style="text-align: center;">发表时间</th>
+            <th width="180" style="text-align: center;">发表时间</th>
             <th width="230" style="text-align: center;">操作</th>
         </tr>
         @foreach($posts as $post)
             <tr>
                 <td>{{ $post->title }}</td>
                 <td style="text-align: center;">
-                    <span class="label label-info" style="width: 80px;display: inline-block;">{{ $post->category->name }}</span>
+                    <span class="label label-info"
+                          style="width: 80px;display: inline-block;">{{ $post->category->name }}</span>
                 </td>
                 <td style="text-align: center;">{{ $post->created_at }}</td>
                 <td style="text-align: center;">
                     <div class="btn-group btn-group-sm" role="group">
                         <button type="button" class="btn btn-info">
-                            <span class="glyphicon glyphicon-info-sign"></span> 查看
+                            <span class="glyphicon glyphicon-info-sign" id="check"></span> 查看
                         </button>
                         <button type="button" class="btn btn-primary">
                             <span class="glyphicon glyphicon-pencil"></span> 更新
@@ -33,4 +34,10 @@
     </table>
 
     {{ $posts->links() }}
+@endsection
+
+@section('scripts')
+    <script>
+        $('#check').popover()
+    </script>
 @endsection
